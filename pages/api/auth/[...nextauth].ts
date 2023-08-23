c2b04620a38ee170b2694b4f8d758591627639e7
v-suganandham-arumugam <118755775+v-suganandham-arumugam@users.noreply.github.com>
2023-08-23 16:08:47 +0100

    is development true

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-const isDevelopment = process.env.NODE_ENV === "development";
+const isDevelopment = true;
 
 const providers = [];
 
